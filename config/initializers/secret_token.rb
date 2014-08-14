e90dc911bf2bb7d8750247864fa35a61df6d8bd8
Yen Adams Sokama-Neuyam <yen@Scholarsup.com>
2014-08-15 01:55:16 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prayer::Application.config.secret_key_base = '4db20becb128fb00b766ee7af1a3444c500b35d619a784dd5a75ad778406ee16ba4e262755282a82efe4971a5744be129a7d37dc4fcc1b63da2dbcdcf5125d30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prayer::Application.config.session_store :cookie_store, key: '_prayer_session'
